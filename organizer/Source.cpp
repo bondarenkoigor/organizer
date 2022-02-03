40e39f673fea13b6b27a3d70a7f1a40e89839737
Игорь Бондаренко <Bond_fh54@student.itstep.org>
2022-02-03 12:48:43 +0200

    organizer

--- organizer/Source.cpp
+++ organizer/Source.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<exception>
-
+#include<conio.h>
 struct Node
 {
 	int day, month, year;
@@ -10,7 +10,7 @@ struct Node
 	Node(int day, int month, int year, std::string text, Node* next, Node* prev) : day(day), month(month), year(year), text(text), next(next), prev(prev) {}
 };
 
-class OrganizerExcept : std::exception
+class OrganizerExcept : public std::exception
 {
 private:
 	const char* error;
@@ -26,6 +26,20 @@ class Organizer
 {
 private:
 	Node* head;
+
+	Node* find(int day, int month, int year)
+	{
+		if (this->head == nullptr) throw OrganizerExcept("no elements\n");
+		Node* iter = this->head;
+		while (iter != nullptr)
+		{
+			if (iter->day == day && iter->month == month && iter->year == year) break;
+			iter = iter->next;
+		}
+		if (iter == nullptr) throw OrganizerExcept("no such date\n");
+		return iter;
+	}
+
 public:
 	Organizer()
 	{
@@ -33,7 +47,7 @@ public:
 	}
 	void add(int day, int month, int year, std::string text)
 	{
-		if (day > 31 || month > 12 || day < 1 || month < 1) throw OrganizerExcept("wrong date");
+		if (day > 31 || month > 12 || day < 1 || month < 1) throw OrganizerExcept("wrong date\n");
 		if (this->head == nullptr)
 		{
 			this->head = new Node(day, month, year, text, nullptr, nullptr);
@@ -46,17 +60,88 @@ public:
 		}
 		iter->next = new Node(day, month, year, text, nullptr, iter);
 	}
+	void deleteElement(int day, int month, int year)
+	{
+		Node* toDelete = find(day, month, year);
+
+		toDelete->prev->next = toDelete->next;
+		if (toDelete->next != nullptr) toDelete->next->prev = toDelete->prev;
+		delete toDelete;
+	}
+	void redact(int day, int month, int year, std::string str)
+	{
+		Node* toRedact = this->find(day, month, year);
+		toRedact->text = str;
+	}
+	void print()
+	{
+		if (this->head == nullptr) throw OrganizerExcept("no elements\n");
+		Node* iter = this->head;
+		while (iter != nullptr)
+		{
+			std::cout << iter->day << "." << iter->month << "." << iter->year << "\t" << iter->text << "\n";
+			iter = iter->next;
+		}
+	}
 };
 
 int main()
 {
 	Organizer org;
-	try
+	while (true)
 	{
-		org.add(0, 123, 42387, "text");
-	}
-	catch (OrganizerExcept except)
-	{
-		std::cout << except.what() << "\n";
+		try
+		{
+			system("cls");
+			std::cout << "1 - add\n"
+				<< "2 - delete\n"
+				<< "3 - redact\n"
+				<< "4 - print\n"
+				<< "Esc - exit\n";
+			switch (_getch())
+			{
+				std::cin.ignore();
+			case '1':
+			{
+				std::cout << "enter date and text:\n";
+				int d, m, y;
+				std::string str;
+				std::cin >> d >> m >> y;
+				std::cin >> str;
+
+				org.add(d, m, y, str);
+				break;
+			}
+			case '2':
+			{
+				std::cout << "enter date:\n";
+				int d, m, y;
+				std::cin >> d >> m >> y;
+				org.deleteElement(d, m, y);
+				break;
+			}
+			case '3':
+			{
+				std::cout << "enter date:\n";
+				int d, m, y;
+				std::cin >> d >> m >> y;
+				std::cout << "enter new text:\n";
+				std::string str;
+				std::cin >> str;
+				org.redact(d, m, y, str);
+				break;
+			}
+			case '4':
+				org.print();
+				break;
+
+			case 27: return 0;
+			}
+		}
+		catch (std::exception& error)
+		{
+			std::cout << error.what();
+		}
+		system("pause");
 	}
 }
